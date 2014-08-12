e0021cc1984a314ccfc42235c5d193e1ee8b0d0d
Bruce Hauman <bhauman@gmail.com>
2014-08-12 16:06:03 -0400

    bumped version for new figwheel and devcards

--- src/leiningen/new/devcards/project.clj
+++ src/leiningen/new/devcards/project.clj
@@ -7,9 +7,9 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2202"]]
 
-  :profiles { :dev {:dependencies [[devcards "0.1.1-SNAPSHOT"]]
+  :profiles { :dev {:dependencies [[devcards "0.1.2-SNAPSHOT"]]
                     :plugins [[lein-cljsbuild "1.0.3"]
-                              [lein-figwheel "0.1.3-SNAPSHOT"]] }}
+                              [lein-figwheel "0.1.4-SNAPSHOT"]] }}
 
   :source-paths ["src"]
 

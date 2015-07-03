ddad6f19b310fc27eb98b0c5938c0867e34a299f
Bruce Hauman <bhauman@gmail.com>
2015-07-03 09:32:06 -0400

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject devcards/lein-template "0.1.4"
+(defproject devcards/lein-template "0.2.0"
   :description "A Leinigen template for devcards"
   :url "https://github.com/bhauman/devcards-template"
   :license {:name "Eclipse Public License"

e7d2fb38b98ed0627d0f4a457ac26c2aee876684
Bruce Hauman <bhauman@gmail.com>
2015-10-18 14:39:06 -0400

    updating template

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject devcards/lein-template "0.2.0-8"
+(defproject devcards/lein-template "0.2.0-9"
   :description "A Leinigen template for Devcards"
   :url "https://github.com/bhauman/devcards-template"
   :license {:name "Eclipse Public License"
--- src/leiningen/new/devcards/project.clj
+++ src/leiningen/new/devcards/project.clj
@@ -6,10 +6,10 @@
 
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.122"]
-                 [devcards "0.2.0-3"]
-                 [sablono "0.3.4"]
-                 #_[org.omcljs/om "0.8.8"]
-                 #_[reagent "0.5.0"]]
+                 [devcards "0.2.0-7"]
+                 [sablono "0.3.6"]
+                 #_[org.omcljs/om "0.9.0"]
+                 #_[reagent "0.5.1"]]
 
   :plugins [[lein-cljsbuild "1.1.0"]
             [lein-figwheel "0.4.0"]]

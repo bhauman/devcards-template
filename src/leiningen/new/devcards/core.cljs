89b3a93a2ed3e86035732b95dee2b600abc70235
Joe Selman <jsselman@google.com>
2016-07-12 21:54:27 -0700

    Fix react render method call syntax
    Fixes #19

--- src/leiningen/new/devcards/core.cljs
+++ src/leiningen/new/devcards/core.cljs
@@ -15,7 +15,7 @@
   ;; conditionally start the app based on whether the #main-app-area
   ;; node is on the page
   (if-let [node (.getElementById js/document "main-app-area")]
-    (js/React.render (sab/html [:div "This is working"]) node)))
+    (.render js/ReactDOM (sab/html [:div "This is working"]) node)))
 
 (main)
 

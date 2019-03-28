42d222633f2f340094b6fce008ff2980fc2c3792
Adam Frey <adam@adamfrey.me>
2019-03-28 17:27:53 -0400

    Upgrade dependencies

--- src/leiningen/new/devcards/project.clj
+++ src/leiningen/new/devcards/project.clj
@@ -11,21 +11,21 @@
               ;; under Java 9
   :jvm-opts ["--add-modules" "java.xml.bind"]{{/java9?}}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.229"]
-                 [devcards "0.2.3"]
-                 [sablono "0.7.4"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.520"]
+                 [devcards "0.2.6"]
+                 [sablono "0.8.5"]
 
                  ;; need to specify this for sablono
                  ;; when not using devcards
-                 [cljsjs/react "15.3.1-0"]
-                 [cljsjs/react-dom "15.3.1-0"]
-                 #_[org.omcljs/om "1.0.0-alpha46"]
-                 #_[reagent "0.6.0"]
+                 [cljsjs/react "16.8.3-0"]
+                 [cljsjs/react-dom "16.8.3-0"]
+                 #_[org.omcljs/om "1.0.0-beta1"]
+                 #_[reagent "0.8.1"]
                  ]
 
-  :plugins [[lein-figwheel "0.5.9"]
-            [lein-cljsbuild "1.1.5" :exclusions [org.clojure/clojure]]]
+  :plugins [[lein-figwheel "0.5.18"]
+            [lein-cljsbuild "1.1.7" :exclusions [org.clojure/clojure]]]
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled"
                                     "target"]
@@ -63,13 +63,13 @@
 
   :figwheel { :css-dirs ["resources/public/css"] }
 
-  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.2"]
-                                  [figwheel-sidecar "0.5.9"]
-                                  [com.cemerick/piggieback "0.2.1"]]
+  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.10"]
+                                  [figwheel-sidecar "0.5.18"]
+                                  [cider/piggieback "0.4.0"]]
                    ;; need to add dev source path here to get user.clj loaded
                    :source-paths ["src" "dev"]
                    ;; for CIDER
                    ;; :plugins [[cider/cider-nrepl "0.12.0"]]
                    :repl-options {; for nREPL dev you really need to limit output
                                   :init (set! *print-length* 50)
-                                  :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}})
+                                  :nrepl-middleware [cider.piggieback/wrap-cljs-repl]}}})
